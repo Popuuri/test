6c184f5b40a4570bd4f0969f508b15db9ac375e9
popuuri <popurimourya@gmail.com>
2023-10-06 10:31:49 +0530

    first commit

---
+++ frontend2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import C1 from './components/C1';
+import C2 from './components/c2';
+
+function App() {
+  return (
+    <div>
+      {/* <h1>State and props</h1>
+      <C1 gift="watch" /> */}
+      <C2 />
+    </div>
+  );
+}
+
+export default App;
+
